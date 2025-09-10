8c0f9e17ae2ceea84cbff26d035b25a88d1e6784
Richmondfreeheart <kuamerichmond@gmail.com>
2025-09-10 01:36:20 +0000

    d

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
@@ -16,7 +17,24 @@ function App() {
       <MainContent />
       <Footer />
     </div>
+    
   );
 }
+
+  return (
+    <div>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+
+      <UserProfile 
+        name="Bob" 
+        age="30" 
+        bio="Enjoys coding and traveling" 
+      />
+    </div>
+  );
 
 export default App;
