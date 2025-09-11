fdaf34f63aa7e93f7ec2f7b69c71a681311c4358
Richmondfreeheart <kuamerichmond@gmail.com>
2025-09-11 13:54:27 +0000

    new

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,6 +3,12 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -35,6 +41,18 @@ function App() {
         bio="Enjoys coding and traveling" 
       />
     </div>
+  );
+
+   return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 
 export default App;
