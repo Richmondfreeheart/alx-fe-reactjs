55b5bf61f00891ed1d3a989dca4a9c99c2532359
Richmondfreeheart <kuamerichmond@gmail.com>
2025-09-11 13:21:31 +0000

    a

--- alx-react-app-props/src/UserInfo.jsx
+++
@@ -1,9 +0,0 @@
-// src/UserInfo.jsx
-import UserProfile from './UserProfile'; // still correct if UserProfile is in components
-
-
-function UserInfo() {
-  return <UserDetails />;
-}
-
-export default UserInfo;
