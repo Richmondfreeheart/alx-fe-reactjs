1594212d704283b5f5fe33ceefbf6a19a0a6efab
Richmondfreeheart <kuamerichmond@gmail.com>
2025-09-13 09:36:56 +0000

    recipe

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,40 @@
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+
+    return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+    
+  );
+}
+
+  return (
+    <div>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+
+      <UserProfile 
+        name="Bob" 
+        age="30" 
+        bio="Enjoys coding and traveling" 
+      />
+    </div>
+  );
+
+export default App;
