0ff1fba6aa2d81a8d995a1d33f349cb54fbef366
Richmondfreeheart <kuamerichmond@gmail.com>
2025-09-13 09:24:43 +0000

    a

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { useRecipeStore } from "../recipeStore";
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Recipes</h1>
+      {recipes.map((recipe) => (
+        <div key={recipe.id} style={{ marginBottom: "10px" }}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+          <Link to={`/recipes/${recipe.id}`}>View Details</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
