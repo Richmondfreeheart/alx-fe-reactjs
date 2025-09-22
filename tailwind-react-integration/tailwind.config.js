fa55884793cca1b08de7057d5915c0379a51a459
Richmondfreeheart <142225053+Richmondfreeheart@users.noreply.github.com>
2025-09-22 02:13:14 +0000

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Ensure all React component files are included
+    "./index.html", 
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this includes all paths where Tailwind is used
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
