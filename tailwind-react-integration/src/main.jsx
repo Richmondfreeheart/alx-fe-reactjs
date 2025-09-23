4d4e6a8c5e2e212dfe1efa855519330dc3b8224a
Richmondfreeheart <142225053+Richmondfreeheart@users.noreply.github.com>
2025-09-23 11:19:03 +0000

    Update main.jsx

--- tailwind-react-integration/src/main.jsx
+++ tailwind-react-integration/src/main.jsx
@@ -2,7 +2,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './index.css'   // <-- must import the CSS
+import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
