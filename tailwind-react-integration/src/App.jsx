25c7c16961df58a33142735c832b881da08e7c7c
Richmondfreeheart <142225053+Richmondfreeheart@users.noreply.github.com>
2025-09-23 11:26:46 +0000

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.jsx
 import React from 'react'
+import UserProfile from "./components/UserProfile";
 
 export default function App() {
   return (
@@ -7,6 +8,12 @@ export default function App() {
       <div className="w-full max-w-md bg-white shadow-lg rounded-xl p-6">
         <h1 className="text-2xl font-semibold mb-2">Tailwind + Vite + React</h1>
         <p className="text-gray-600 mb-4">If you see nice spacing and fonts, Tailwind is active.</p>
+
+         return (
+    <div>
+      <UserProfile />
+    </div>
+  )
 
         <div className="flex gap-3">
           <button className="flex-1 py-2 rounded-md bg-sky-600 text-white font-medium hover:bg-sky-700">
